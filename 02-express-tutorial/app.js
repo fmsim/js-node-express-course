743afbe7f0cbd3923d625b8fb62e6591bcdf4de8
Frank Simon <f.m.sim@t-online.de>
2023-01-15 20:28:05 +0100

    examples with different middleware conzepts. added logger.js and authorize.js files. installed morgan third party middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+const logger = require("./logger");
+const authorize = require("./authorize");
 
 // req => middleware => res
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+// 1. use vs route
+// 2. options - our own / express / third party
 
-app.get("/", logger, (req, res) => {
+// app.use([logger, authorize]);
+// app.use(express.static("./public"));
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
   res.send("Home");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About");
+});
+
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+
+app.get("/api/items", (req, res) => {
+  // @ts-ignore
+  console.log(req.user);
+  res.send("Items");
 });
 
 app.listen(5000, () => {
