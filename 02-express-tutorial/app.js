da326fb7651f483195fdbe1dfdbb5289cd57727b
Frank Simon <f.m.sim@t-online.de>
2023-01-15 20:41:59 +0100

    modified app.js with http method get

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,35 +1,9 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
+let { people } = require("./data");
 
-const logger = require("./logger");
-const authorize = require("./authorize");
-
-// req => middleware => res
-
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// app.use([logger, authorize]);
-// app.use(express.static("./public"));
-app.use(morgan("tiny"));
-
-app.get("/", (req, res) => {
-  res.send("Home");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-
-app.get("/api/items", (req, res) => {
-  // @ts-ignore
-  console.log(req.user);
-  res.send("Items");
+app.get("/api/people", (req, res) => {
+  res.status(200).send({ success: true, data: people });
 });
 
 app.listen(5000, () => {
